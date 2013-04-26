672b5d316d0914614e64bfd201241c3b23929504
Arto Kaikkonen <kaikkonen.arto@gmail.com>
2013-04-26 16:39:24 +0300

    tehtävä2 arraylistillä + pari testiä

--- viikko6/IntJoukkoSovellus/src/main/java/ohtu/intjoukkosovellus/IntJoukko.java
+++ viikko6/IntJoukkoSovellus/src/main/java/ohtu/intjoukkosovellus/IntJoukko.java
@@ -1,142 +1,95 @@
 package ohtu.intjoukkosovellus;
 
+import java.util.ArrayList;
+
 public class IntJoukko {
 
-    public final static int KAPASITEETTI = 5, // aloitustalukon koko
-            OLETUSKASVATUS = 5;  // luotava uusi taulukko on 
-    // näin paljon isompi kuin vanha
-    private int kasvatuskoko;     // Uusi taulukko on tämän verran vanhaa suurempi.
-    private int[] ljono;      // Joukon luvut säilytetään taulukon alkupäässä. 
-    private int alkioidenLkm;    // Tyhjässä joukossa alkioiden_määrä on nolla. 
+    private ArrayList<Integer> ljono;
 
     public IntJoukko() {
-        this(KAPASITEETTI,OLETUSKASVATUS);
+        ljono = new ArrayList<Integer>();
     }
-    
 
     public IntJoukko(int kapasiteetti) {
-        this(kapasiteetti,OLETUSKASVATUS);
+        this();
     }
 
     public IntJoukko(int kapasiteetti, int kasvatuskoko) {
-        if (kapasiteetti < 0) {
-            throw new IndexOutOfBoundsException("Kapasitteetti väärin");//heitin vaan jotain :D
-        }
-        if (kasvatuskoko < 0) {
-            throw new IndexOutOfBoundsException("kapasiteetti2");//heitin vaan jotain :D
-        }
-        ljono = new int[kapasiteetti];
-        this.kasvatuskoko = kasvatuskoko;
+        this();
     }
 
     public boolean lisaa(int luku) {
-        if (!kuuluu(luku)) {
-            ljono[alkioidenLkm] = luku;
-            alkioidenLkm++;
-            if (alkioidenLkm % ljono.length == 0) {
-                kasvataTaulukonKokoa();
-            }
-            return true;
+        if (!ljono.contains(luku)) {
+            return ljono.add(luku);
         }
         return false;
-    }
-
-    private void kasvataTaulukonKokoa() {
-        int[] uusiLukujono = new int[alkioidenLkm + kasvatuskoko];
-        for (int i = 0; i < ljono.length; i++) {
-            uusiLukujono[i] = ljono[i];
-        }
-        ljono = uusiLukujono;
     }
 
     public boolean kuuluu(int luku) {
-        for (int i = 0; i < alkioidenLkm; i++) {
-            if (luku == ljono[i]) {
-                return true;
-            }
-        }
-        return false;
-
-    }
-
-    private int haeIndeksi(int luku) {
-        for (int i = 0; i < ljono.length; i++) {
-            if (ljono[i] == luku) {
-                return i;
-            }
-        }
-        return -1;
+        return ljono.contains(luku);
     }
 
     public boolean poista(int luku) {
-        int i = haeIndeksi(luku);
-        ljono[i] = ljono[alkioidenLkm - 1];
-        ljono[alkioidenLkm - 1] = 0;
-        alkioidenLkm -= 1;
-        return false;
+        Integer poistettava = (Integer) luku;
+        return ljono.remove(poistettava);
+
     }
 
     private IntJoukko teeKopio() {
-        IntJoukko x = new IntJoukko();
-        int[] luvut = this.toIntArray();
-        for (int i = 0; i < this.alkioidenLkm; i++) {
-            x.lisaa(luvut[i]);            
+        IntJoukko uusiJoukko = new IntJoukko();
+        for (Integer x : ljono) {
+            uusiJoukko.lisaa(x);
         }
-        return x;
+        return uusiJoukko;
     }
-    
+
     public int mahtavuus() {
-        return alkioidenLkm;
+        return ljono.size();
     }
 
     @Override
     public String toString() {
         String tuotos = "{";
-        for (int i = 0; i < alkioidenLkm - 1; i++) {
-            tuotos += ljono[i];
-            tuotos += ", ";
-        }
-        tuotos += ljono[alkioidenLkm - 1];
+        tuotos += ljono.toString().substring(1, ljono.toString().length() - 1);
         tuotos += "}";
         return tuotos;
     }
 
     public int[] toIntArray() {
-        int[] taulu = new int[alkioidenLkm];
+        int[] taulu = new int[ljono.size()];
         for (int i = 0; i < taulu.length; i++) {
-            taulu[i] = ljono[i];
+            taulu[i] = ljono.get(i);
         }
         return taulu;
     }
 
-    public static IntJoukko yhdiste(IntJoukko a, IntJoukko b) {        
+    public static IntJoukko yhdiste(IntJoukko a, IntJoukko b) {
         IntJoukko x = a.teeKopio();
-        int[] bTaulu = b.toIntArray();
-        for (int i = 0; i < bTaulu.length; i++) {
-            x.lisaa(bTaulu[i]);
+        for (int i : b.getLjono()) {
+            x.lisaa(i);
         }
         return x;
     }
 
     public static IntJoukko leikkaus(IntJoukko a, IntJoukko b) {
         IntJoukko x = new IntJoukko();
-        int[] aTaulu = a.toIntArray();
-        for (int i = 0; i < aTaulu.length; i++) {
-            if (b.kuuluu(aTaulu[i])) {
-                x.lisaa(aTaulu[i]);
+        for (int i : b.getLjono()) {
+            if (a.kuuluu(i)) {
+                x.lisaa(i);
             }
         }
         return x;
     }
 
     public static IntJoukko erotus(IntJoukko a, IntJoukko b) {
-        IntJoukko x = new IntJoukko();
-        int[] aTaulu = a.toIntArray();
-        for (int i = 0; i < aTaulu.length; i++) {
-            if (!b.kuuluu(aTaulu[i])) {
-                x.lisaa(aTaulu[i]);
-            }
+        IntJoukko x = a.teeKopio();
+        for (int i : b.getLjono()) {
+            x.poista(i);
         }
         return x;
+    }
+
+    public ArrayList<Integer> getLjono() {
+        return ljono;
     }
 }
--- viikko6/IntJoukkoSovellus/src/test/java/ohtu/intjoukkosovellus/JoukkoOperaatiotTest.java
+++ viikko6/IntJoukkoSovellus/src/test/java/ohtu/intjoukkosovellus/JoukkoOperaatiotTest.java
@@ -32,4 +32,31 @@ public class JoukkoOperaatiotTest {
         
         return joukko;
     }
+    @Test
+        public void testLeikkaus() {
+        IntJoukko eka = teeJoukko(1,2,3,4,5);
+        IntJoukko toka = teeJoukko(3,4,5,6,7,8,9);
+        
+        IntJoukko tulos = IntJoukko.leikkaus(eka, toka);
+        int[] vastauksenLuvut = tulos.toIntArray();
+        Arrays.sort(vastauksenLuvut);
+        
+        int[] odotettu = {3,4,5};
+        
+        assertArrayEquals(odotettu, vastauksenLuvut);        
+    } 
+    
+        @Test
+        public void testErotus() {
+        IntJoukko eka = teeJoukko(1,2,3,4,5);
+        IntJoukko toka = teeJoukko(3,4,5,6,7,8,9);
+        
+        IntJoukko tulos = IntJoukko.erotus(eka, toka);
+        int[] vastauksenLuvut = tulos.toIntArray();
+        Arrays.sort(vastauksenLuvut);
+        
+        int[] odotettu = {1,2};
+        
+        assertArrayEquals(odotettu, vastauksenLuvut);        
+    } 
 }
